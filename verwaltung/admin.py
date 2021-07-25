bf167ec032c809d48c0741fb6489d33384ee0ec2
reto <info@rutishauser-ag.ch>
2021-07-25 18:35:16 +0200

    develop

--- verwaltung/admin.py
+++ verwaltung/admin.py
@@ -7,33 +7,33 @@ from django.contrib import admin
 from django.core.serializers.json import DjangoJSONEncoder
 from django.db.models import Count, Sum, Min, Max
 
-from .models import Mieter, Mietobjekt, Nebenkosten, Mietzinsprofil, \
-    Mietzins, Year, Unterhalt, Mietzinseingaenge, Building
+from .models import Renter, RentalProperty, ExtraCosts, RentProfile, \
+    Rent, Year, Investment, RentReceipts, Building
 
 
 # Register your models here.
-@admin.register(Mieter)
-class MieterAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Mieter._meta.fields]
+@admin.register(Renter)
+class RenterAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Renter._meta.fields]
     list_filter = ('activ',)
 
 admin.site.register(Year)
 
 
-@admin.register(Mietobjekt)
-class MietobjektAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Mietobjekt._meta.fields]
+@admin.register(RentalProperty)
+class RentalPropertyAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in RentalProperty._meta.fields]
     list_filter = ('building',)
 
 # ---- Table Nebenkosten
-@admin.register(Nebenkosten)
-class NebenkostenAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Nebenkosten._meta.fields]
+@admin.register(ExtraCosts)
+class ExtraCostsAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in ExtraCosts._meta.fields]
 
 
-@admin.register(Mietzins)
-class MietzinsAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Mietzins._meta.fields]
+@admin.register(Rent)
+class RentAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Rent._meta.fields]
 
 
 @admin.register(Building)
@@ -42,9 +42,9 @@ class BuildingAdmin(admin.ModelAdmin):
    # fields = ('image_tag',)
    # readonly_fields = ('image_tag',)
 
-@admin.register(Unterhalt)
-class UnterhaltAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Unterhalt._meta.fields]
+@admin.register(Investment)
+class InvestmentAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Investment._meta.fields]
     # set the Filter option
     list_filter = ('mietobject',)
 
@@ -57,27 +57,32 @@ class UnterhaltAdmin(admin.ModelAdmin):
         """
         # Aggregate new subscribers per day
         invest_data_object = (
-           Unterhalt.objects.values("mietobject__name","mietobject__building__name").annotate(y=Sum("betrag")).order_by("-mietobject")
+           Investment.objects.values("mietobject__name", "mietobject__building__name").annotate(y=Sum("amount")).order_by("-mietobject")
         )
         miete_data_object = (
-             Mietzins.objects.values("mietobject__name","mietobject__building__name").annotate(y=Sum("rent")).order_by("-mietobject")
+             Rent.objects.values("mietobject__name", "mietobject__building__name").annotate(y=Sum("rent")).order_by("-mietobject")
         )
         invest_data_building = (
-              Unterhalt.objects.values("mietobject__building__name").annotate(y=Sum("betrag"))
+              Investment.objects.values("mietobject__building__name").annotate(y=Sum("amount"))
         )
         miete_data_building = (
-             Mietzins.objects.values("mietobject__building__name").annotate(y=Sum("rent"))
+             Rent.objects.values("mietobject__building__name").annotate(y=Sum("rent"))
         )
         print('invest_data_object:'+ str(invest_data_object))
         print('miete_data_object:'+ str(miete_data_object))
-        print('invest_data_building:'+ str(invest_data_building))
-        print('miete_data_building:' + str(miete_data_building))
+      #  print('invest_data_building:'+ str(invest_data_building))
+      #  print('miete_data_building:' + str(miete_data_building))
 
         # Serialize and attach the chart data to the template context
         as_json_invest_data_object = json.dumps(list(invest_data_object), cls=DjangoJSONEncoder)
         as_json_miete_data_object = json.dumps(list(miete_data_object), cls=DjangoJSONEncoder)
         as_json_invest_data_building = json.dumps(list(invest_data_building), cls=DjangoJSONEncoder)
         as_json_miete_data_building = json.dumps(list(miete_data_building), cls=DjangoJSONEncoder)
+
+        print('as_json_invest_data_object:'+ str(as_json_invest_data_object))
+        print('miete_data_object:'+ str(as_json_miete_data_object))
+       # print('as_json_invest_data_building:'+ str(as_json_invest_data_building))
+       # print('miete_data_building:' + str(as_json_miete_data_building))
 
         extra_context = extra_context or {"invest_data_object": as_json_invest_data_object,
                                           "miete_data_object": as_json_miete_data_object,
@@ -86,10 +91,10 @@ class UnterhaltAdmin(admin.ModelAdmin):
 
         return super().changelist_view(request, extra_context=extra_context)
 
-# admin.site.register(Nebenkosten_Typ)
-@admin.register(Mietzinsprofil)
-class MietzinsprofilAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Mietzinsprofil._meta.fields]
+
+@admin.register(RentProfile)
+class RentProfileAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in RentProfile._meta.fields]
 
     def formfield_for_manytomany(self, db_field, request, **kwargs):
         """
@@ -99,16 +104,16 @@ class MietzinsprofilAdmin(admin.ModelAdmin):
         :param kwargs:
         :return:
         """
-        if db_field.name == "miete":
-            kwargs["queryset"] = Mietzins.objects.filter(aktiv= True)
+        if db_field.name == "rent":
+            kwargs["queryset"] = Rent.objects.filter(aktiv= True)
             print(str(kwargs["queryset"]))
         return super().formfield_for_manytomany(db_field, request, **kwargs)
 
 
-# ---- Table Mietzinseingaenge
-@admin.register(Mietzinseingaenge)
-class MietzinseingaengeAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Mietzinseingaenge._meta.fields]
+# ---- Table RentReceipts
+@admin.register(RentReceipts)
+class RentReceiptsAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in RentReceipts._meta.fields]
 
     #  list_filter  = ('mieter')
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
@@ -120,14 +125,14 @@ class MietzinseingaengeAdmin(admin.ModelAdmin):
         :return:
         """
         if db_field.name == "mietzinsprofil":
-            kwargs["queryset"] = Mietzinsprofil.objects.filter(activ=True)
+            kwargs["queryset"] = RentProfile.objects.filter(activ=True)
             # print(" query : " + str(kwargs["queryset"]))
-        return super(MietzinseingaengeAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+        return super(RentReceiptsAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
     def changelist_view(self, request, extra_context=None):
         # Aggregate new subscribers per day
         chart_data = (
-            Mietzinseingaenge.objects.values("year__typ").annotate(y=Sum("betrag")).order_by("-year")
+            RentReceipts.objects.values("year__typ").annotate(y=Sum("amount")).order_by("-year")
         )
         print('Mieteinnahmen chart:'+ str(chart_data))
 
--- verwaltung/models.py
+++ verwaltung/models.py
@@ -15,29 +15,42 @@ class Building(models.Model):
     image_tag.short_description = 'Image'
     image_tag.allow_tags = True
 
+    class Meta:
+        # set the Name of the Table at admin site
+        verbose_name_plural = "Gebäude"
+        verbose_name = "Gebäude"
+
     def __str__(self):
         return self.name
 
 
-class Mietobjekt(models.Model):
+class RentalProperty(models.Model):
     name  = models.CharField(verbose_name="Objektname",max_length=200, default=0)
     building  = models.ForeignKey(Building, on_delete=models.CASCADE, default=0)
     description = models.TextField(verbose_name="Beschreibung", max_length=1024 * 2)
     photo = models.ImageField(verbose_name="Bild", default=0, blank = True)
 
+    class Meta:
+        # set the Name of the Table at admin site
+        verbose_name_plural = "Mietobjekt"
+        verbose_name = "Mietobjekt"
+
     def __str__(self):
         return self.name+' '+self.building.name
 
 
-class Mietzins(models.Model):
-    #mietobjekt = models.ForeignKey(Mietobjekt, on_delete=models.CASCADE)
+class Rent(models.Model):
     description = models.CharField(verbose_name="Beschreibung",max_length=200)
     rent = models.DecimalField(verbose_name="Mietpreis (SFR)",max_digits=6, decimal_places=2)
-    # mieter = models.ForeignKey(Mieter, on_delete=models.CASCADE, default=0)
     start_date = models.DateField(default=timezone.now())
     end_date = models.DateField(default=timezone.now())
-    mietobject = models.ForeignKey(Mietobjekt, on_delete=models.CASCADE, default=0)
+    mietobject = models.ForeignKey(RentalProperty, on_delete=models.CASCADE, default=0)
     aktiv = models.BooleanField(default=False)
+
+    class Meta:
+        # set the Name of the Table at admin site
+        verbose_name_plural = "Mietzins"
+        verbose_name = "Mietzins"
 
     def __str__(self):
 
@@ -47,22 +60,31 @@ class Mietzins(models.Model):
 class Year(models.Model):
     typ = models.IntegerField(default=2010)
 
+    class Meta:
+        # set the Name of the Table at admin site
+        verbose_name_plural = "Jahr"
+        verbose_name = "Jahr"
+
     def __str__(self):
         return str(self.typ)
 
-class Nebenkosten(models.Model):
-    typ =  models.CharField(max_length=10, choices=(('Heizung', 'Heizung',),
+class ExtraCosts(models.Model):
+    typ = models.CharField(max_length=10, choices=(('Heizung', 'Heizung',),
                                                         ('Wasser', 'Wasser'),
                                                        ('Allg. NK', 'Allg. NK')))
-    betrag = models.DecimalField(max_digits=6, decimal_places=2)
-    a_conto =  models.CharField(max_length=10, choices=(('a', 'à conto',),
+    amount = models.DecimalField(max_digits=6, decimal_places=2)
+    a_conto = models.CharField(max_length=10, choices=(('a', 'à conto',),
                                          ('p', 'pauschal')), default='a')
+    class Meta:
+        # set the Name of the Table at admin site
+        verbose_name_plural = "Nebenkosten"
+        verbose_name = "Nebenkosten"
 
     def __str__(self):
-        return str(self.typ)+' '+str(self.betrag)+' '+str(self.a_conto)
+        return str(self.typ) +' ' + str(self.amount) + ' ' + str(self.a_conto)
 
 
-class Mieter(models.Model):
+class Renter(models.Model):
     company = models.CharField(verbose_name="Firma", max_length=200,blank = True)
     first_name = models.CharField(verbose_name="Vorname",max_length=200)
     last_name = models.CharField(verbose_name="Nachname", max_length=200)
@@ -77,6 +99,11 @@ class Mieter(models.Model):
     last_name_2nd = models.CharField(verbose_name="Zweitmieter Nachname", max_length=200, blank = True)
     activ = models.BooleanField(verbose_name="aktiv", default=False)
 
+    class Meta:
+        # set the Name of the Table at admin site
+        verbose_name_plural = "Mieter"
+        verbose_name = "Mieter"
+
     def __str__(self):
         """
             used for ForeignKey dropDown
@@ -88,33 +115,43 @@ class Mieter(models.Model):
                                                     self.last_name)
 
 
-class Mietzinsprofil(models.Model):
-    mieter = models.ForeignKey(Mieter, on_delete=models.CASCADE,  default =0)
-    miete = models.ManyToManyField(Mietzins)
-    nebenkosten = models.ManyToManyField(Nebenkosten)
+class RentProfile(models.Model):
+    mieter = models.ForeignKey(Renter, on_delete=models.CASCADE, default =0)
+    miete = models.ManyToManyField(Rent)
+    nebenkosten = models.ManyToManyField(ExtraCosts)
     start_date = models.DateField(default=timezone.now())
     end_date = models.DateField(default=timezone.now())
     activ = models.BooleanField(verbose_name="aktiv", default=False)
 
+    class Meta:
+        # set the Name of the Table at admin site
+        verbose_name_plural = "Mietzinsprofil"
+        verbose_name = "Mietzinsprofil"
+
     def __str__(self):
         # total Miete und Nebenkosten
         total = self.miete.aggregate(total_score=Sum('rent'))['total_score'] + \
-                self.nebenkosten.aggregate(total_score=Sum('betrag'))['total_score']
+                self.nebenkosten.aggregate(total_score=Sum('amount'))['total_score']
         return str(self.mieter)+' SFR '+str(total)
 
 
-class Unterhalt(models.Model):
+class Investment(models.Model):
     project = models.CharField(verbose_name="Projekt", max_length=200)
-    mietobject = models.ForeignKey(Mietobjekt, on_delete=models.CASCADE, default =0)
+    mietobject = models.ForeignKey(RentalProperty, on_delete=models.CASCADE, default =0)
     description = models.TextField(verbose_name="Beschreibung", max_length=1024 * 2)
-    betrag = models.DecimalField(verbose_name="Investition (SFR)",max_digits=10, decimal_places=2)
+    amount = models.DecimalField(verbose_name="Investition (SFR)", max_digits=10, decimal_places=2)
     datum = models.DateField(default=timezone.now())
 
+    class Meta:
+        # set the Name of the Table at admin site
+        verbose_name_plural = "Unterhalt"
+        verbose_name = "Unterhalt"
+
     def __str__(self):
-        return str(self.mietobject) + ' ' + str(self.project) + ' ' + str(self.betrag)
+        return str(self.mietobject) + ' ' + str(self.project) + ' ' + str(self.amount)
 
 
-class Mietzinseingaenge(models.Model):
+class RentReceipts(models.Model):
     datum = models.DateField(default=timezone.now())
     # ,input_formats=["%Y-%m-%d", ]
     month = models.CharField(max_length=10, choices=(('JA', 'Januar',),
@@ -129,11 +166,17 @@ class Mietzinseingaenge(models.Model):
                                                      ('OK', 'Oktober'),
                                                      ('NO', 'November'),
                                                      ('DE', 'Dezember'),))
-    mieter = models.ForeignKey(Mieter, on_delete=models.CASCADE)
-    mietzinsprofil = models.ForeignKey(Mietzinsprofil, on_delete=models.CASCADE, default =0)
-    betrag = models.IntegerField(default=0)
+    mieter = models.ForeignKey(Renter, on_delete=models.CASCADE)
+    mietzinsprofil = models.ForeignKey(RentProfile, on_delete=models.CASCADE, default =0)
+    amount = models.IntegerField(default=0)
     year  = models.ForeignKey(Year, on_delete=models.CASCADE, default=0)
+    description = models.TextField(verbose_name="Bemerkungen", max_length=1024 * 2, blank = True)
+
+    class Meta:
+        # set the Name of the Table at admin site
+        verbose_name_plural = "Mietzinseingänge"
+        verbose_name = "Mietzinseingänge"
 
     def __str__(self):
-        return str(self.mieter) + ' ' + str(self.betrag) + ' ' + str(self.datum)
+        return str(self.mieter) + ' ' + str(self.amount) + ' ' + str(self.datum)
 
